63302483b263682bfa85ee94940db25302304d6e
cawfee-art <133345306+cawfee-art@users.noreply.github.com>
2025-01-25 17:39:17 +0100

    credits

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,9 +2,13 @@ extends Control
 
 
 
-func _on_quit_pressed():
-	get_tree().quit()
 
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+
+
+func _on_credit_pressed():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
