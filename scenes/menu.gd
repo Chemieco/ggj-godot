02e6eaf1328f43c8566a967b3b03ef12ece6d2aa
HairaNight <157826367+HairaNight@users.noreply.github.com>
2025-01-25 22:17:35 +0100

    add menu music + sound file fix

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -9,6 +9,9 @@ func _on_start_pressed():
 
 
 
-
 func _on_credit_pressed():
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_settings_pressed():
+	PauseMenu.show_pause_menu()
